c10bec5d0719b27cea050f604b5bb3b9e57d2393
Ruth K <kennjeri2005@gmail.com>
2019-06-29 10:37:29 -0400

    added files

---
+++ 2main.js
@@ -0,0 +1,15 @@
+// alert("hello stay away")
+// prompt("If you like this page type yes")
+// confirm("Confirm if you are 18")
+var name = "Friends";
+alert( "Hello "+ name + "!" + ' Welcome to My Page');
+
+// var age = 19
+// confirm("are you 18 +?")
+// if(confirm){
+//   alert("Come in!")
+// }
+// function showAlert(){
+//   alert("Welcome");
+// }
+showAlert();
